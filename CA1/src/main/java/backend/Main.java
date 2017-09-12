bd74978ad94c393ac186cc771abf4efa70bd1766
Unknown <benjaminlrasmussen@gmail.com>
2017-09-12 14:39:54 +0200

    Populate from main

--- CA1/src/main/java/backend/Main.java
+++ CA1/src/main/java/backend/Main.java
@@ -29,16 +29,20 @@ public class Main
         List<Phone> phones = new ArrayList();
         phones.add(new Phone(66666666, "The devil's number"));
         Address add = new Address("Lil-marco", new CityInfo(3230, "Græsted"), "There is no additional info");
-        facade.addPerson(new Person(0, phones, add, "Satan", "Lucifer"));
+        Person satan = (new Person(0, phones, add, "Satan", "Lucifer"));
 
         List<Phone> phones2 = new ArrayList();
         phones.add(new Phone(22332233, "Bluuup"));
         Address add2 = new Address("Lil-Banana", new CityInfo(2840, "Holte"), "Joejoe");
-        facade.addPerson(new Person(1, phones2, add2, "God", "Christ"));
+        Person god = (new Person(1, phones2, add2, "God", "Christ"));
 
         List<Phone> phones3 = new ArrayList();
         phones.add(new Phone(88778877, "Google.com"));
         Address add3 = new Address("Google-street 17", new CityInfo(2880, "Lyngby"), "Brrrr");
         Company google = new Company(0, phones3, add3, "Google", "Nice google!", 99889988, 860, 1000000.00);
+        
+        facade.addPerson(satan);
+        facade.addPerson(god);
+        facade.addCompany(google);
     }
 }
