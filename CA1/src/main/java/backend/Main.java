aadcc84a2ecc4e5c7ad0387ef65d348e43f5b41c
Mikkel <mikkel@mediagram.dk>
2017-09-12 14:17:57 +0200

    commit

--- CA1/src/main/java/backend/Main.java
+++ CA1/src/main/java/backend/Main.java
@@ -6,5 +6,7 @@ import javax.persistence.Persistence;
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaPU");
+        
+        
     }
 }
