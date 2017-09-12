f045ca80ec2d7ea17010809212608511d23ac817
Joachim Hansen <craciez@live.dk>
2017-09-12 13:36:46 +0200

    PersonsResources almost done with 1.0

--- CA1/src/main/java/rest/PersonResource.java
+++ CA1/src/main/java/rest/PersonResource.java
@@ -1,11 +1,14 @@
 package rest;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import facade.CompanyMapper;
 import facade.Facade;
 import facade.PersonMapper;
 import java.util.List;
 import entity.Person;
+import entity.Phone;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.ws.rs.core.Context;
@@ -15,6 +18,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -24,11 +28,12 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("person")
 public class PersonResource {
+
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaPU");
 
     Facade facade = new Facade(new PersonMapper(emf), new CompanyMapper());
     Gson gs = new Gson();
-    
+
     @Context
     private UriInfo context;
 
@@ -40,6 +45,7 @@ public class PersonResource {
 
     /**
      * Retrieves representation of an instance of rest.PersonResource
+     *
      * @return an instance of java.lang.String
      */
     @Path("complete")
@@ -50,8 +56,54 @@ public class PersonResource {
         return gs.toJson(pl);
     }
 
+    @Path("complete/{id}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson(@PathParam("id") int id) {
+        Person p = facade.getPerson(id);
+        return gs.toJson(p);
+    }
+
+    @Path("conactinfo")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getContactinfo() {
+        List<Person> pl = facade.getAllPersons();
+        JsonArray ja = new JsonArray();
+        for (Person person : pl) {
+            JsonObject jo = new JsonObject();
+            jo.addProperty("Email", "none email");
+            List<Phone> phoneL = person.getPhones1();
+            for (int i = 0; i < phoneL.size(); i++) {
+                jo.addProperty("phoneNumber" + i, phoneL.get(i).getNumber());
+            }
+            ja.add(jo);
+        }
+        return gs.toJson(ja);
+    }
+
+    @Path("conactinfo/{id}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getContactinfo(@PathParam("id") int id) {
+        Person p = facade.getPerson(id);
+        JsonArray ja = new JsonArray();
+        JsonObject jo = new JsonObject();
+        
+        jo.addProperty("Email", "none email");
+        
+        List<Phone> phoneL = p.getPhones1();
+        for (int i = 0; i < phoneL.size(); i++) {
+            jo.addProperty("phoneNumber" + i, phoneL.get(i).getNumber());
+        }
+        ja.add(jo);
+
+        return gs.toJson(ja);
+    }
+
     /**
      * PUT method for updating or creating an instance of PersonResource
+     *
      * @param content representation for the resource
      */
     @PUT
