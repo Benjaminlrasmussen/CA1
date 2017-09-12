ab6c8f8430d69910fa06924eea0462c46665ba4a
Unknown <benjaminlrasmussen@gmail.com>
2017-09-12 12:49:39 +0200

    Removed garbage comments

--- CA1/src/main/java/facade/IPersonMapper.java
+++ CA1/src/main/java/facade/IPersonMapper.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package facade;
 
 import entity.Person;
 import java.util.List;
 
-/**
- *
- * @author Peter
- */
 public interface IPersonMapper {
     
     public Person getPerson(int id);
