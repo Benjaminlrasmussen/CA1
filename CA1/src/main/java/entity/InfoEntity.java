5a43f348a3c06b40cd13dfddd8881448beab9a1b
Peter Rasmussen <leonhardt_88@live.dk>
2017-09-14 13:25:50 +0200

    minor bugs implementation

--- CA1/src/main/java/entity/InfoEntity.java
+++ CA1/src/main/java/entity/InfoEntity.java
@@ -20,7 +20,6 @@ public class InfoEntity implements Serializable
 
     private static final long serialVersionUID = 1L;
     @Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(unique = true)
